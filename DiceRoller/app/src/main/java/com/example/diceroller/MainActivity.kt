bd662d8ac75108b68da025af7a294f148a29a445
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-09-29 16:09:25 -0600

    Remove Dice class

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -93,15 +93,3 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-/**
- * Dice with a fixed number of sides.
- */
-class Dice(private val numSides: Int) {
-
-    /**
-     * Do a random dice roll and return the result.
-     */
-    fun roll(): Int {
-        return (1..numSides).random()
-    }
-}
